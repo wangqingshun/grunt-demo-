1b47439f7d6a9f5d02c06dbe76b029427c418a1c
wangqingshun <2184801857@qq.com>
2019-03-10 14:26:09 +0800

    css静态代码检查

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,15 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    mochacli: {
+    csslint: {
       options: {
-        reporter: 'spec',
-        bail: true
+        csslintrc: '.csslintrc',
       },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
-      }
+    src:['*.css']
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.registerTask('default', ['csslint']); 
 };
 
