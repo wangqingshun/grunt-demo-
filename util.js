2142679a025a3c3c0caf3ebe3dd5195a27e40b60
wangqingshun <2184801857@qq.com>
2019-03-11 19:31:53 +0800

    前端单元测试

---
+++ util.js
@@ -0,0 +1,10 @@
+function rectangle(){
+    return {
+	'perimeter':function(w,h){
+	    return z*(Number(w)+Number(h));
+        },
+	'area':function(w,h){
+	    return Number(w)*Number(h);
+	}
+    };
+}
