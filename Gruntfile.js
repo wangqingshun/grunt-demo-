ecda5ccbc736f3ba94a8c6e1de1443035251f76a
wangqingshun <2184801857@qq.com>
2019-03-12 14:39:30 +0800

    html压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,20 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    mochacli: {
+    htmlmin: {
       options: {
-        reporter: 'spec',
-        bail: true
+        collapseWhitespace: true,
+        preserveLineBreaks: false
       },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('default', ['htmlmin']);
 };
 
