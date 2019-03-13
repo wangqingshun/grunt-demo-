0a681e1721528da8015abafea53e4cb358ff5ef7
wangqingshun <2184801857@qq.com>
2019-03-13 16:46:28 +0800

    接口测试

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http'),
+      url = require('url'),
+      qs = require('querystring');
+
+http.createServer((req,res)=>{
+      console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
+      console.log(req.Headers);
+      console.log();
+    
+      var addr = url.parse(req.url);
+      var rect = qs.parse(addr.query);
+
+      var r={
+          'area':Number(rect.width)*Number(rect.height),
+          'perimeter':2*(Number(rect.width)+Number(rect.height))
+      }
+
+      res.end(JSON.stringify(r));
+}).listen(8080);
+
+process.on('stop',function(){
+      console.log("服务停止");
+      process.exit();
+})
